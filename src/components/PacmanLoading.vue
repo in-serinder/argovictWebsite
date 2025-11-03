1d423d3fac676fff27122d954d28f507e97af9bc
Serinder <serinder@163.com>
2025-11-03 13:19:28 +0800

    Pacman Loading

---
+++ src/components/PacmanLoading.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="pacman-loading">
+
+        <!-- Pacman  -->
+        <div class="pacman">
+            <div class="pacman-eye"></div>
+            <div class="pacman-mouth"></div>
+        </div>
+
+
+        <!-- Bean -->
+        <ul class="pacman-bean">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+
+
+    </div>
+</template>
+
+
+<script lang="ts" setup>
+import '@/style/pacman-loading.css'
+</script>
--- src/views/MyProject.vue
+++ src/views/MyProject.vue
@@ -3,6 +3,8 @@
         <div class="projectOfOne " v-for="item in projectList" :key="item.name">
             <ProjectOne :project="item" />
         </div>
+        <!-- Pacman Loading -->
+        <!-- <PacmanLoading v-if="aspacmanLoading" /> -->
 
         <!-- 占位 -->
         <div class="placeholder-bottom"></div>
@@ -12,24 +14,46 @@
 <script setup lang="ts">
 import '@/style/projectPage.css'
 import ProjectOne from '@/components/ProjectOne.vue';
+import PacmanLoading from '@/components/PacmanLoading.vue'
 import { ref, onMounted } from 'vue'
 import type { ProjectItem } from '@/interface';
 import { useGetContentFromServerStore } from '@/stores/getContentFromServer'
 
+
 const getContentFromServerStore = useGetContentFromServerStore()
 
 const projectList = ref<ProjectItem[]>([]);
+// const aspacmanLoading = ref<boolean>(true)
+
 
 document.title = 'Argovict - My Project'
 
+// const pacmanExit = () => {
+//     console.log('pacmanExit')
+//     // pacmanLoading.value = false
+//     const pacmanLoading = document.querySelector('.pacman-loading') as HTMLElement
+//     pacmanLoading.classList.add('pacman-loading-exit')
+//     aspacmanLoading.value = false
+// }
 
 
 onMounted(() => {
     // getProjectList();
     getContentFromServerStore.getProjectList().then((res) => {
         projectList.value = res as ProjectItem[]
+        // if (projectList.value.length === 0) {
+        //     pacmanExit()
+        // }
     })
+
+    // setTimeout(() => {
+
+    //     pacmanExit()
+
+    // }, 3000);
 });
+
+
 
 
 
