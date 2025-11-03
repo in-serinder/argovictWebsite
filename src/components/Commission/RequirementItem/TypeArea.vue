a5562cd48c640acc5a12e5c80565b365469bc90e
Serinder <serinder@163.com>
2025-11-03 16:30:15 +0800

    Improve

---
+++ src/components/Commission/RequirementItem/TypeArea.vue
@@ -0,0 +1,32 @@
+<!-- 对数量相关的适配 -->
+<template>
+    <div class="commission-type-area">
+        <div class="commission-type-area-item">
+            <!-- 标题 -->
+            <p>TypeArea</p>
+            <!-- 描述 -->
+            <img src="@/assets/svg/unkonw.svg" alt="">
+            <!-- 带有按键 -->
+            <div class="commission-type-area-as" v-show="isAs">
+                <div><img src="@/assets/svg/sub.svg" alt=""> <input type="text"><img src="@/assets/svg/add.svg" alt="">
+                </div>
+
+            </div>
+            <!-- 直接键入 -->
+            <div class="commission-type-area-direct" v-show="!isAs">
+
+                <input type="text">
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import '@/style/Commission/Components/commissionTypeArea.css'
+import { ref } from 'vue'
+
+const isAs = ref<boolean>(true)
+
+
+</script>
--- src/views/Commission/Type/Exmaple.vue
+++ src/views/Commission/Type/Exmaple.vue
@@ -7,6 +7,7 @@
         <CheckBox></CheckBox>
         <Radio></Radio>
         <SingleRadio></SingleRadio>
+        <TypeArea></TypeArea>
         <Remarks></Remarks>
     </div>
 </template>
@@ -16,5 +17,6 @@ import CheckBox from '@/components/Commission/RequirementItem/CheckBox.vue';
 import Radio from '@/components/Commission/RequirementItem/Radio.vue';
 import SingleRadio from '@/components/Commission/RequirementItem/SingleRadio.vue';
 import Remarks from '@/components/Commission/RequirementItem/Remarks.vue';
+import TypeArea from '@/components/Commission/RequirementItem/TypeArea.vue';
 
 </script>
